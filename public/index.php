42c6b9433988a0997c6de1d70394c587cd284df9
Zsolt Vizi <vizi.g.zsolt@gmail.com>
2019-10-02 11:44:31 +0300

    Add nginx and php as docker containers

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+phpinfo();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Hello Docker2</h1>
+</body>
+</html>
